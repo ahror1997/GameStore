14c24e34cfa621899edcfbdce78774ba4fd3e9b0
Akhror Ziyodinov <ahror.ziyodinov@gmail.com>
2024-03-02 03:33:02 +0100

    initial commit

---
+++ GameStore.API/DTOs/CreateGameDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.API.DTOs;
+
+public record class CreateGameDTO(
+    [Required][StringLength(50)] string Name,
+    [Required][StringLength(20)] string Genre,
+    [Range(1, 100)] decimal Price,
+    DateOnly ReleaseDate
+);
---
+++ GameStore.API/Endpoints/GamesEndpoints.cs
@@ -0,0 +1,92 @@
+﻿using GameStore.API.DTOs;
+
+namespace GameStore.API.Endpoints;
+
+public static class GamesEndpoints
+{
+    const string GetGameEndpointName = "GetGame";
+    private static readonly List<GameDTO> games = new List<GameDTO>() {
+        new GameDTO(
+            1,
+            "Street Fighter II",
+            "Fighting",
+            19.99M,
+            new DateOnly(1992, 7, 15)
+        ),
+        new GameDTO(
+            2,
+            "Final Fantasy XIV",
+            "RP",
+            59.99M,
+            new DateOnly(2010, 9, 30)
+        ),
+        new GameDTO(
+            3,
+            "FIFA 23",
+            "Sports",
+            69.99M,
+            new DateOnly(2022, 9, 27)
+        )
+    };
+
+    public static RouteGroupBuilder MapGamesEndpoints(this WebApplication app)
+    {
+        var group = app.MapGroup("games").WithParameterValidation();
+
+        // GET /games
+        group.MapGet("", () => games);
+
+        // GET /games/1
+        group.MapGet("{id}", (int id) => {
+            var game = games.Find(game => game.Id == id);
+
+            return game is null ? Results.NotFound() : Results.Ok(game);
+        })
+        .WithName(GetGameEndpointName);
+
+        // POST /games
+        group.MapPost("", (CreateGameDTO newGame) =>
+        {
+            GameDTO game = new GameDTO(
+                games.Count + 1,
+                newGame.Name,
+                newGame.Genre,
+                newGame.Price,
+                newGame.ReleaseDate
+            );
+
+            games.Add(game);
+
+            return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id}, game);
+        });
+
+        // PUT games/1
+        group.MapPut("{id}", (int id, UpdateGameDTO updatedGame) => {
+            var index = games.FindIndex(game => game.Id == id);
+
+            if (index == -1)
+            {
+                return Results.NotFound();
+            }
+
+            games[index] = new GameDTO(
+                id,
+                updatedGame.Name,
+                updatedGame.Genre,
+                updatedGame.Price,
+                updatedGame.ReleaseDate
+            );
+
+            return Results.NoContent();
+        });
+
+        // DELETE /games/1
+        group.MapDelete("{id}", (int id) => {
+            games.RemoveAll(game => game.Id == id);
+
+            return Results.NoContent();
+        });
+
+        return group;
+    }
+}
