3e5d480164b23aa32131532efd147758f1993d3a
Akhror Ziyodinov <ahror.ziyodinov@gmail.com>
2024-03-03 19:39:56 +0100

    health checks

---
+++ GameStore.API/Health/DatabaseHealthCheck.cs
@@ -0,0 +1,35 @@
+﻿using GameStore.API.Database;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace GameStore.API.Health
+{
+    public class DatabaseHealthCheck : IHealthCheck
+    {
+        private readonly IDbConnectionFactory _dbConnectionFactory;
+
+        public DatabaseHealthCheck(IDbConnectionFactory dbConnectionFactory)
+        {
+            _dbConnectionFactory = dbConnectionFactory;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+                using var command = connection.CreateCommand();
+                command.CommandText = "SELECT 1";
+                command.ExecuteScalar();
+
+                return HealthCheckResult.Healthy();
+            }
+            catch (Exception exception)
+            {
+
+                return HealthCheckResult.Unhealthy(exception: exception);
+            }
+        }
+    }
+}
--- GameStore.API/Program.cs
+++ GameStore.API/Program.cs
@@ -1,19 +1,46 @@
 using GameStore.API.Data;
+using GameStore.API.Database;
 using GameStore.API.Endpoints;
+using GameStore.API.Health;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// logging
 builder.Services.AddHttpLogging(options => {});
 
-var connectionString = builder.Configuration.GetConnectionString("GameStore");
-builder.Services.AddSqlite<GameStoreContext>(connectionString);
+// db connection string
+var connectionString = builder.Configuration.GetConnectionString("GameStore:PostgreSQL");
+
+builder.Services.AddSingleton<IDbConnectionFactory>(_ =>
+    new SqliteConnectionFactory(builder.Configuration.GetConnectionString("GameStore:SQLite")!));
+
+// health checks
+builder.Services.AddHealthChecks()
+    .AddCheck<DatabaseHealthCheck>("sqlite")
+    .AddNpgSql(connectionString!);
+
+//builder.Services.AddSqlite<GameStoreContext>(connectionString);
+
+builder.Services.AddNpgsql<GameStoreContext>(connectionString);
 
 var app = builder.Build();
 
+// logging
 app.UseHttpLogging();
 
+
+// health checks
+app.UseHealthChecks("/_health", new HealthCheckOptions()
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+// database migration
 await app.MigrateDbAsync();
 
+// endpoints
 app.MapGamesEndpoints();
 app.MapGenresEndpoints();
 
